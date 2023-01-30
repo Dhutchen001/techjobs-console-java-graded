20460594ea31a4adb787f78881330650d3f7a315
DH001 <dhutchen01@gmail.com>
2023-01-29 23:04:49 -0600

    finished part 2 and accidentally also did part 3 in the process of making it work

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -5,10 +5,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -98,8 +95,19 @@ public class JobData {
         // load data, if not already loaded
         loadData();
 
-        // TODO - implement this method
-        return null;
+        ArrayList<HashMap<String, String>> result = new ArrayList<>();
+
+        for (HashMap<String, String> row : allJobs) {
+
+            Collection<String> aValue = row.values();
+            String searchableValue = aValue.toString().toLowerCase();
+            if (searchableValue.contains(value.toLowerCase())){
+                if (!result.contains(row)) {
+                    result.add(row);
+                }
+            }
+        }
+        return result;
     }
 
     /**
